64e9cf7b9890d0d826b5b969fbd0a3676ccdfbf1
Leoni Terto <terto.leoni@gmail.com>
2024-09-05 15:33:13 -0300

    .

---
+++ qrcode/geradorQrcode.php
@@ -0,0 +1,19 @@
+<?php
+include_once "./phpqrcode-master/qrlib.php";
+
+function connectDb()
+{
+    $server = "192.168.3.35"; $user = "dalconito"; $pass = "HelloWorld"; $database = "fabrica";
+    $conn = mysqli_connect($server, $user, $pass, $database);
+    return $conn;}
+
+function Query($idQrCode)
+{$conn = connectDb();
+    $selectQuery = "SELECT * FROM qrcode where id = $idQrCode";
+    $resultQuery = mysqli_query($conn, $selectQuery);
+    $returnQuery = mysqli_fetch_assoc($resultQuery);
+    $conn->close(); return $returnQuery;}
+
+function gerarQrCode($texto)
+{QRcode::png($texto, 'qrcode.jpeg', QR_ECLEVEL_L, 10);
+    return QRcode::png($texto);}
---
+++ scannerQr.php
@@ -0,0 +1,55 @@
+<!DOCTYPE html>
+<html lang="pt-BR">
+<head>
+    <?php include_once "./phpqrcode-master/qrlib.php" ?>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Escaner de QrCode</title>
+    <style>
+        #reader {
+            width: 100%;
+            height: auto;
+            border: 1px solid black;
+        }
+
+        .escondido{display: none;}
+        .encontrado{font-size: 1.8rem; margin: 10px;}
+        .validar{font-size: 2rem; color: aqua; background-color: burlywood;
+        padding: 10px; margin: 40px; align-self: center;}
+        .divBtn{text-align: center;}
+    </style>
+</head>
+<body>
+    <h1>Escaner de QrCode</h1>
+    <div id="reader"></div>
+    <p id="qrCodeParagh" class="escondido encontrado">QrCode Encontrado!</p>
+    <div class="divBtn">
+        <a href="" id="linkqrCode"><button disabled class="validar" id="btnValidar">Validar</button></a>
+    </div>
+    
+    <script src="https://unpkg.com/html5-qrcode/html5-qrcode.min.js"></script>
+    <script>
+        var linkQr = document.getElementById("linkqrCode")
+        var btnValidar =document.getElementById("btnValidar")
+        function onScanSuccess(decodedText, decodedResult) {
+            linkQr.setAttribute("href", decodedText);
+            document.getElementById("qrCodeParagh").style.display = "block";
+            btnValidar.disabled = false
+        }
+
+        function onScanError(errorMessage) {console.log('Erro de escaneamento: ', errorMessage);}
+
+        Html5Qrcode.getCameras().then(devices => {
+            // Selecionar a câmera padrão (por exemplo, a frontal)
+            const cameraId = devices[1].id; // Ou use devices[n] para outra câmera
+            
+            const html5QrCode = new Html5Qrcode("reader");
+            html5QrCode.start(cameraId, 
+                {fps: 30, qrbox: 250},
+                onScanSuccess, onScanError
+            );
+        }).catch(err => {console.error("Erro ao obter câmeras: ", err);});
+    </script>
+
+</body>
+</html>
