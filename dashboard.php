66290126537fe7882fc63d37d7d1cab537efa696
Leoni Terto <terto.leoni@gmail.com>
2024-09-23 12:34:18 -0300

    aaaa

--- dashboard.php
+++ dashboard.php
@@ -1,5 +1,7 @@
 <?php
 session_start();
+$login = $_SESSION['login'];
+$opra = selectUserDash($login);
 ?>
 <!DOCTYPE html>
 <html lang="pt-br">
@@ -9,13 +11,10 @@ session_start();
     <title>Dashboard</title>
 </head>
 <body>
-    <!-- Exibe o nome do usuário logado -->
     <h1>Olá <?php echo htmlspecialchars($_SESSION['login']); ?></h1>
 
-    <!-- Botão para enviar dados via JavaScript -->
     <button id="botaoEnvia">Enviar Dados</button>
 
-    <!-- Inclui o script que contém o JavaScript para a interação com o servidor -->
     <script src="./public/js/dashboard.js" defer></script>
 </body>
 </html>
